516b187151f2a056c4b8e4e32b736f47d4126e06
MASSYNAS HAMLAT <massynas.hamlat@atos.net>
2019-06-02 16:35:41 +0200

    'deploy'

---
+++ precache-manifest.5af836b81c7dd95b0ceb0ef74b936550.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa_push_arthur/robots.txt"
+  },
+  {
+    "revision": "98731c0aba889845bdbf",
+    "url": "/pwa_push_arthur/js/chunk-vendors.2fd57737.js"
+  },
+  {
+    "revision": "9956a4ac283c0dcae4a7",
+    "url": "/pwa_push_arthur/js/app.51876162.js"
+  },
+  {
+    "revision": "47e8f077e284185f807c",
+    "url": "/pwa_push_arthur/js/about.e62c8732.js"
+  },
+  {
+    "revision": "8ca10c649ae9a5cee236bca6096673a1",
+    "url": "/pwa_push_arthur/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa_push_arthur/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9956a4ac283c0dcae4a7",
+    "url": "/pwa_push_arthur/css/app.267862f8.css"
+  },
+  {
+    "revision": "47e8f077e284185f807c",
+    "url": "/pwa_push_arthur/css/about.7b280509.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/pwa_push_arthur/precache-manifest.5af836b81c7dd95b0ceb0ef74b936550.js", "/pwa_push_arthur/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/pwa_push_arthur/workbox-v3.6.3"});
+workbox.core.setCacheNameDetails({
+    prefix: "pwa_push_arthur"
+});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerNavigationRoute('/index.html');
+
+self.addEventListener("message", event => {
+    console.log(`[Service Worker] Message recieived: ${event.data}`)
+    if (event.data === "skipWaiting") {
+        self.skipWaiting();
+    }
+})
